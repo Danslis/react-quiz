a268b1baf26e66c0a0f2172a5d8ea359445b7122
Dart <Danslis@yandex.ru>
2026-03-07 10:44:21 +0300

    Список вопросов

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,12 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import classes from './Quiz.module.css';
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 const Quiz = () => {
-  const [quiz, setQuiz] = useState([]);
+  const [quiz] = useState([
+    {
+      answers: [
+        { text: 'Вопрос 1' },
+        { text: 'Вопрос 2' },
+        { text: 'Вопрос 3' },
+        { text: 'Вопрос 4' }
+      ]
+    }
+  ]);
 
   return (
     <div className={classes.Quiz}>
-      <h1>Quiz</h1>
+      <div className={classes.QuizWrapper}>
+        <h1>Ответьте на все вопросы</h1>
+
+        <ActiveQuiz
+          answers={quiz[0].answers}
+        />
+      </div>
     </div>
   );
 };
