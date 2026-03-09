81b98a5451e36cdec3a8ace7fafd971cc9ff887c
Dart <Danslis@yandex.ru>
2026-03-09 16:33:08 +0300

    Страница авторизации

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const loginHandler = () => {
+    console.log('Login')
+  }
+
+  const registerHandler = () => {
+    console.log('Register')
+  }
+
+  const submitHandler = event => {
+    event.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className={classes.Auth}>
+      <div>
+        <h1>Авторизация</h1>
+
+        <form onSubmit={submitHandler} className={classes.AuthForm}>
+          <input 
+            type="email" 
+            placeholder="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input 
+            type="password" 
+            placeholder="Пароль"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <Button type="success" onClick={loginHandler}>
+            Войти
+          </Button>
+
+          <Button type="primary" onClick={registerHandler}>
+            Зарегистрироваться
+          </Button>
+        </form>
+      </div>
     </div>
   )
 }
