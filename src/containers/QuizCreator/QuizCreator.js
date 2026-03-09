4e06ecefa140115c28ecae1f42e9cf0dc1d0734f
Dart <Danslis@yandex.ru>
2026-03-09 23:13:08 +0300

    Создание тестов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import classes from './QuizCreator.module.css'
+import Button from '../../components/UI/Button/Button'
 
 const QuizCreator = () => {
+  const submitHandler = event => {
+    event.preventDefault()
+  }
+
+  const addQuestionHandler = () => {
+    console.log('Add question')
+  }
+
+  const createQuizHandler = () => {
+    console.log('Create quiz')
+  }
+
   return (
-    <div>
-      <h1>QuizCreator</h1>
+    <div className={classes.QuizCreator}>
+      <div>
+        <h1>Создание теста</h1>
+
+        <form onSubmit={submitHandler}>
+          <input type="text" placeholder="Вопрос" />
+          <hr />
+          <input type="text" placeholder="Вариант 1" />
+          <input type="text" placeholder="Вариант 2" />
+          <input type="text" placeholder="Вариант 3" />
+          <input type="text" placeholder="Вариант 4" />
+
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+
+          <Button type="primary" onClick={addQuestionHandler}>
+            Добавить вопрос
+          </Button>
+
+          <Button type="success" onClick={createQuizHandler}>
+            Создать тест
+          </Button>
+        </form>
+      </div>
     </div>
   )
 }
