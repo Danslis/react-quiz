3649ac6bf3f3c16b9e428f99227c7ce4404e6585
Dart <Danslis@yandex.ru>
2026-03-21 22:16:41 +0300

    Настройка проекта

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './QuizList.module.css'
 import {NavLink} from 'react-router-dom'
+import axios from 'axios'
 
 const QuizList = () => {
+  useEffect(() => {
+    axios.get('https://react-quiz-a964c-default-rtdb.firebaseio.com/quiz.json')
+      .then(response => {
+        console.log(response)
+      })
+  }, [])
+
   const renderQuizes = () => {
     return [1, 2, 3].map((quiz, index) => (
       <li key={index}>
